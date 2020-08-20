000b6f12827e89f6d248210fad78b751c8eced12
Vinicius Bandeira <bandeiravinicius0@live.com>
2020-08-19 21:31:14 -0300

    Create mongo db connection and first insert

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/nodeapi", {useNewUrlParser: true, useUnifiedTopology: true});
+
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
+
 app.get("/", (req,res) => {
-    res.send("hello world");
+    Product.create({
+        title: "New title",
+         description: "title auto generated",
+        url: "http://www.google.com"
+        });
+    return res.send("product created");
 });
 
 app.listen(3000);
